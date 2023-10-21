548cf1fa44936b904ac2f19d35bcf4696c07fe1a
Md Saayed <mdsayed.contact@gmail.com>
2023-10-21 22:23:46 +0600

    Banner text issues solved

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
                 <div className='order-2 md:order-1 md:items-start items-center justify-start md:justify-center md:items-start flex flex-col'>
                     <h2 className='text-xl'>BEST PRICE : $866</h2>
                     <h1 className='font-extrabold text-5xl text-center md:text-left'>NEW SMART PHONE</h1>
-                    <p className='text-2xl font-semibold mt-2'>SAMSUNG GRAND 6</p>
+                    <p className='text-2xl font-semibold mt-3 md:mt-2'>SAMSUNG GRAND 6</p>
                     <p className='my-4 text-center md:text-left'>There are many variations of passages of phones availables, but the majority have suffered alteration in some form.</p>
                     <button className='bg-blue-700 mt-6 rounded py-2 px-8 text-white '>BUY NOW</button>
                 </div>
